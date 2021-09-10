de10b308127161207f88c9eb854a35feb724f89a
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2021-09-10 16:31:14 +0900

    一旦

--- ucpp5/escape.cpp
+++ ucpp5/escape.cpp
@@ -4,8 +4,8 @@ int main() {
 	std::cout << "\x1b[31mHOGE" << std::endl;
 	std::cout << "\x1b[0mFOO" << std::endl;
 	std::cout << "\x1b[32mFOO" << std::endl;
-	for(int color = 0; color < 7; color++) {
-		std::cout << "\x1b[" << (30 + color) << "mABCDEFG" << std::endl;
+	for(int color = 0; color < 90; color++) {
+		std::cout << "\x1b[" << color << "mABCDEFG" << std::endl;
 	}
 	//C:\Users
 	//こんにちは
